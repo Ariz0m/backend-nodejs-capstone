6483e869396b51900dc6397ae8be5bf3c302d609
Arizom <157034570+Ariz0m@users.noreply.github.com>
2026-08-08 00:28:54 -0600

    Fix some lint issues

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -42,14 +42,13 @@ app.use('/api/secondchance/items', secondChanceItemsRoutes)
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/secondchance/search', searchRoutes)
 
-
 // Global Error Handler
 app.use((err, req, res, next) => {
   console.error(err)
   res.status(500).send('Internal Server Error')
 })
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.send('Inside the server')
 })
 
