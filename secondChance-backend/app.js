5ac6e851cfb80f9b819bed214f15fa500e880ff4
Arizom <157034570+Ariz0m@users.noreply.github.com>
2026-08-08 00:27:52 -0600

    Fix some lint issues

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -17,10 +17,9 @@ app.use('/images', express.static(join(__dirname, 'public', 'images')))
 
 // Connect to MongoDB we just do this one time
 connectToDatabase().then(() => {
-    logger.info('Connected to DB')
+  logger.info('Connected to DB')
 })
-.catch((e) => console.error('Failed to connect to DB', e))
-
+  .catch((e) => console.error('Failed to connect to DB', e))
 
 app.use(express.json())
 
@@ -29,32 +28,31 @@ app.use(express.json())
 // authRoutes Step 2: import the authRoutes and store in a constant called authRoutes
 const authRoutes = require('./routes/authRoutes')
 
-
 const { pinoHttp } = require('pino-http')
 
 app.use(pinoHttp({ logger }))
 
 // Use Routes
 // authRoutes Step 2: add the authRoutes and to the server by using the app.use() method.
-app.use("/api/auth", authRoutes)
+app.use('/api/auth', authRoutes)
 
 // Items API Task 2: add the secondChanceItemsRoutes to the server by using the app.use() method.
-app.use("/api/secondchance/items", secondChanceItemsRoutes)
+app.use('/api/secondchance/items', secondChanceItemsRoutes)
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-app.use("/api/secondchance/search", searchRoutes)
+app.use('/api/secondchance/search', searchRoutes)
 
 
 // Global Error Handler
 app.use((err, req, res, next) => {
-    console.error(err)
-    res.status(500).send('Internal Server Error')
+  console.error(err)
+  res.status(500).send('Internal Server Error')
 })
 
-app.get("/",(req,res)=>{
-    res.send("Inside the server")
+app.get('/', (req,res) => {
+  res.send('Inside the server')
 })
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
