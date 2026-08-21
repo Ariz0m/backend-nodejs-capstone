7296f43ef9b12be3bdc31764c0059fb92bd51325
Arizom <157034570+Ariz0m@users.noreply.github.com>
2026-08-21 11:35:57 -0600

    Fix ESLint indentation and quotes in searchRoutes.js

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -8,28 +8,28 @@ const removeNulls = require('../util/removeNulls')
 
 // Search for gifts
 router.get('/', async (req, res, next) => {
-    try {
-        // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
-        const db = await connectToDatabase()
+  try {
+    // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
+    const db = await connectToDatabase()
 
-        const collection = db.collection("secondChanceItems")
+    const collection = db.collection('secondChanceItems')
 
-        // Initialize the query object
-        const query = removeNulls(req.query)
+    // Initialize the query object
+    const query = removeNulls(req.query)
 
-        // Add the name filter to the query if the name parameter is not empty
-        if (req.query.name) query.name = { $regex: req.query.name, $options: "i" }
+    // Add the name filter to the query if the name parameter is not empty
+    if (req.query.name) query.name = { $regex: req.query.name, $options: 'i' }
 
-        if (req.query.age_years) query.age_years = { $lte: parseInt(req.query.age_years) }
+    if (req.query.age_years) query.age_years = { $lte: parseInt(req.query.age_years) }
 
-        // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant]
-        /** @type SecondChanceItemWithId[] */
-        const gifts = await collection.find(query).toArray()
+    // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant]
+    /** @type SecondChanceItemWithId[] */
+    const gifts = await collection.find(query).toArray()
 
-        res.json(gifts)
-    } catch (e) {
-        next(e)
-    }
+    res.json(gifts)
+  } catch (e) {
+    next(e)
+  }
 })
 
 module.exports = router
