5e3e40a633c5aedefe5a3382cdc8ce293a451e58
Ariz0m <arielviveroshour2@gmail.com>
2026-09-14 21:34:00 -0600

    Develop Back End for Registration

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -1,155 +1,52 @@
-const router = require("express").Router();
-const getCollection = require("../models/getCollection");
-const logger = require("../logger");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = process.env;
-const removeNulls = require("../util/removeNulls");
-const requireString = require("../util/requireString");
-const { body ,validationResult } = require("express-validator")
+const express = require('express');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const connectToDatabase = require('../models/db');
+const router = express.Router();
+const pino = require('pino');  // Import Pino logger
+process.loadEnvFile();
 
-/**
- * @typedef {import('../types/user.d.ts').User} User
- * @typedef {import('../types/user.d.ts').UserWithId} UserWithId
- */
+const logger = pino();  // Create a Pino logger instance
 
-const USER_COLLECTION_NAME = "users";
+//Create JWT secret
+const JWT_SECRET = process.env.JWT_SECRET;
 
-router.post("/register", async (req, res) => {
-    const USER_COLLECTION = await getCollection(USER_COLLECTION_NAME);
+router.post('/register', async (req, res) => {
+    try {
+      //Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+      const db = await connectToDatabase();
+      const collection = db.collection("users");
+      const existingEmail = await collection.findOne({ email: req.body.email });
 
-    /**
-    * @type User
-    */
-    const body = req.body;
-    const { email, password, firstName, lastName } = body;
-
-    if (!email) {
-        logger.error("Email was not provided in the request body");
-        return res.status(400).json({error: "Email is required"});
-    }
-
-    if (!password) {
-        logger.error("Password was not provided in the request body");
-        return res.status(400).json({error: "Password is required"});
-    }
-
-    const existingEmail = await USER_COLLECTION.findOne({ email });
-
-    if (existingEmail) {
-        logger.error(`Email ${email} was already registered`);
-        // Another solution could also be redirect to account recovery or login
-        return res.status(400).json({error: "Email is already registered"});
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-    
-    const newUser = await USER_COLLECTION.insertOne({
-        email,
-        password: hash,
-        firstName,
-        lastName,
-        createdAt: new Date()
-    });
-
-    const payload = {
-       user: {
-        id: newUser.insertedId
-       } 
-    }
-
-    const authToken = jwt.sign(payload, JWT_SECRET);
-
-    logger.info(`User ${email} registered successfully`);
-    res.status(201).json({ email, authToken });
-});
-
-router.post("/login", async (req, res) => {
-    const USER_COLLECTION = await getCollection(USER_COLLECTION_NAME);
-    /**
-    * @type User
-    */
-    const body = req.body;
-    const { email, password } = body;
-
-    if (!email || !password) {
-        logger.error("Email or password was not provided in the request body");
-        return res.status(400).json({error: "Email and password are required"});
-    }
-
-    /**
-    * @type UserWithId
-    */
-    const user = await USER_COLLECTION.findOne({ email });
-    if (!user) {
-        logger.error(`User with email ${email} not found`);
-        return res.status(400).json({error: "Invalid credentials"});
-    }
-
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-        logger.error(`Invalid password for user ${email}`);
-        return res.status(400).json({error: "Invalid credentials"});
-    }
-
-    const payload = {
-        user: {
-            id: user._id.toString()
+        if (existingEmail) {
+            logger.error('Email id already exists');
+            return res.status(400).json({ error: 'Email id already exists' });
         }
-    };
 
-    const authToken = jwt.sign(payload, JWT_SECRET);
+        const salt = await bcryptjs.genSalt(10);
+        const hash = await bcryptjs.hash(req.body.password, salt);
+        const email=req.body.email;
+        const newUser = await collection.insertOne({
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: hash,
+            createdAt: new Date(),
+        });
 
-    logger.info(`User ${email} logged in successfully`);
-    res.status(200).json({ email, firstName: user.firstName, authToken });
-});
+        const payload = {
+            user: {
+                id: newUser.insertedId,
+            },
+        };
 
-router.put("/update", async (req, res) => {
-    const errors = validationResult(req);
-
-	if (!errors.isEmpty()) {
-        logger.error('Validation errors in update request', errors.array());
-        return res.status(400).json({ errors: errors.array() });
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        logger.info('User registered successfully');
+        res.json({ authtoken,email });
+    } catch (e) {
+        logger.error(e);
+        return res.status(500).send('Internal server error');
     }
-
-    const { email } = req.headers;
-
-    if (!email) {
-        logger.error('Email not found in the request headers');
-        return res.status(400).json({ error: "Email not found in the request headers" });
-	}
-
-	const USER_COLLECTION = await getCollection(USER_COLLECTION_NAME);
-
-    const existingUser = await USER_COLLECTION.findOne({ email });
-
-    if (!existingUser) {
-        logger.error('User not found');
-        return res.status(404).json({ error: "User not found" });
-    }
-
-    existingUser.firstName = requireString(req.body.firstName);
-    existingUser.updatedAt = new Date();
-
-    const updatedUser = await USER_COLLECTION.updateOne(
-        { email },
-        { $set: existingUser },
-        { returnDocument: 'after' }
-    );
-
-    const payload = {
-        user: {
-            id: existingUser._id.toString(),
-        },
-    };
-
-    const authtoken = jwt.sign(payload, JWT_SECRET);
-    logger.info('User updated successfully');
-
-    res.status(200).json({ userName: existingUser.firstName, userEmail: existingUser.email, authtoken });
 });
 
 module.exports = router;
